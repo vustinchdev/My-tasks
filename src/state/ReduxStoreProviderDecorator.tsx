f870835c36b979f9f01f5ea88b958993dd6bcbe7
vustinchdev <vustinchdev@gmail.com>
2023-12-05 19:16:57 +0300

    add ReduxStoreProviderDecorator

---
+++ src/state/ReduxStoreProviderDecorator.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { AppRootStateType } from './store'
+import { Provider } from 'react-redux'
+import { combineReducers, legacy_createStore } from 'redux';
+import { v1 } from 'uuid';
+import { tasksReducer } from './tasks-reducer';
+import { todolistsReducer } from './todolists-reducer';
+
+const rootReducer = combineReducers({
+    tasks: tasksReducer,
+    todolists: todolistsReducer
+})
+
+const initialGlobalState = {
+    todolists: [
+        { id: "todolistId1", title: "What to learn", filter: "all" },
+        { id: "todolistId2", title: "What to buy", filter: "all" }
+    ],
+    tasks: {
+        ["todolistId1"]: [
+            { id: v1(), title: "HTML&CSS", isDone: true },
+            { id: v1(), title: "JS", isDone: false }
+        ],
+        ["todolistId2"]: [
+            { id: v1(), title: "Milk", isDone: false },
+            { id: v1(), title: "React Book", isDone: true }
+        ]
+    }
+};
+
+export const storyBookStore = legacy_createStore(rootReducer, initialGlobalState as AppRootStateType);
+
+export const ReduxStoreProviderDecorator = (storyFn: () => React.ReactNode) => {
+    return <Provider store={storyBookStore}>{storyFn()}</Provider>
+}
