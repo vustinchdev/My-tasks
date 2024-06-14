94772ce178cb639d7979f329961902e7c72b279b
vustinchdev <vustinchdev@gmail.com>
2024-06-14 19:57:13 +0300

    refactor: change styles for body

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import Container from "@mui/material/Container"
 import { TodolistsList } from "../features/TodolistsList/ui/TodolistsList"
 import { Routes, Route, Navigate } from "react-router-dom"
