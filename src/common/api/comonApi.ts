c08f3e2eed85e1071207419ccb09a5abc11408ed
vustinchdev <vustinchdev@gmail.com>
2024-09-26 18:50:13 +0300

    feat: add API-KEY

--- src/common/api/comonApi.ts
+++ src/common/api/comonApi.ts
@@ -2,5 +2,8 @@ import axios from "axios"
 
 export const instance = axios.create({
   baseURL: "https://social-network.samuraijs.com/api/1.1/",
+  headers: {
+    'API-KEY': process.env.REACT_APP_API_KEY
+  },
   withCredentials: true,
 })
