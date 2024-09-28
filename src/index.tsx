3a358000b21ba6ece090c4788ad0bc538580e6d9
vustinchdev <vustinchdev@gmail.com>
2024-09-28 12:06:44 +0300

    feat: add basename

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom"
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/My-tasks">
       <App />
     </BrowserRouter>
   </Provider>,
